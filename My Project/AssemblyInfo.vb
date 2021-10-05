8c45fe6bf01ee185ecc6c1b160970581043ccb4c
Jesper Høy <jesper@jhsoftware.dk>
2021-10-05 23:32:38 +0200

    v. 9.1.1

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("9.1.0.0")>
-<Assembly: AssemblyFileVersion("9.1.0.0")>
+<Assembly: AssemblyVersion("9.1.1.0")>
+<Assembly: AssemblyFileVersion("9.1.1.0")>
